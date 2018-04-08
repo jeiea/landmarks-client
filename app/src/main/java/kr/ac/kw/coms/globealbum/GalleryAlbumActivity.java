473fc06ee871eb581466a0934b0fb9d589780070
Strange-1 <35914126+Strange-1@users.noreply.github.com>
2018-04-09 03:08:17 +0900

    added galleryview

---
+++ app/src/main/java/kr/ac/kw/coms/globealbum/GalleryAlbumActivity.java
@@ -0,0 +1,78 @@
+package kr.ac.kw.coms.globealbum;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.MergeCursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+
+class SingleAlbumAdapter extends BaseAdapter {
+    private Activity activity;
+    private ArrayList<HashMap< String, String >> data;
+    public SingleAlbumAdapter(Activity a, ArrayList < HashMap < String, String >> d) {
+        activity = a;
+        data = d;
+    }
+    public int getCount() {
+        return data.size();
+    }
+    public Object getItem(int position) {
+        return position;
+    }
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        SingleAlbumViewHolder holder = null;
+        if (convertView == null) {
+            holder = new SingleAlbumViewHolder();
+            convertView = LayoutInflater.from(activity).inflate(
+                    R.layout.galleryalbum_row, parent, false);
+
+            holder.galleryImage = (ImageView) convertView.findViewById(R.id.galleryImage);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (SingleAlbumViewHolder) convertView.getTag();
+        }
+        holder.galleryImage.setId(position);
+
+        HashMap < String, String > song = new HashMap < String, String > ();
+        song = data.get(position);
+        try {
+
+            Glide.with(activity)
+                    .load(new File(song.get(Function.KEY_PATH))) // Uri of the picture
+                    .into(holder.galleryImage);
+
+
+        } catch (Exception e) {}
+        return convertView;
+    }
+}
+
+
+class SingleAlbumViewHolder {
+    ImageView galleryImage;
+}
