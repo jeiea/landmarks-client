dcc677237b61ffadaa7b0b962f294d1ad4c0e89c
jeiea <solum5013@gmail.com>
2018-04-11 22:17:05 +0900

    Initial zoom fixed

--- app/src/main/java/kr/ac/kw/coms/globealbum/MapActivity.java
+++ app/src/main/java/kr/ac/kw/coms/globealbum/MapActivity.java
@@ -1,15 +1,15 @@
 package kr.ac.kw.coms.globealbum;
 
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.content.Context;
-import android.util.DisplayMetrics;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
 import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -17,16 +17,14 @@ import org.osmdroid.util.TileSystem;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.config.Configuration;
 import org.osmdroid.views.overlay.MinimapOverlay;
-
 
 
 public class MapActivity extends AppCompatActivity {
 
     final double WORLDMAP_ZOOM_LEVEL = 1.1;
 
-    Context context=null;
+    Context context = null;
     MapView map = null;
     IMapController mapController = null;
     MinimapOverlay minimapOverlay = null;
@@ -41,56 +39,54 @@ public class MapActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //가로모드 고정
 
         //osmdroid 초기 구성
-        context= getApplicationContext();
+        context = getApplicationContext();
         Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
-        map = (MapView)findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
 
-        mapConfiguration();
+        map.post(new Runnable() {
+                     @Override
+                     public void run() {
+                         mapConfiguration();
+                     }
+                 }
+        );
     }
+
     private void mapConfiguration() {    //맵 생성 후 초기 설정
         final DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();   //해상도 측정을 위한 객체
 
         map.setTileSource(TileSourceFactory.MAPNIK);    //맵 렌더링 설정
         map.setBuiltInZoomControls(false);
         map.setMultiTouchControls(true);
-        map.setScrollableAreaLimitLatitude(TileSystem.MaxLatitude-4, -TileSystem.MaxLatitude+35, 0);
-        map.setScrollableAreaLimitLongitude(-TileSystem.MaxLongitude+15, TileSystem.MaxLongitude+10, 0);
-        double dimenRatio = dm.widthPixels / (double)dm.heightPixels; // 화면비율
-//        double mapRatio = 360.0 / (85 * 2); // 위도, 경도에 따른 지도비율 추정치
+        map.setScrollableAreaLimitLatitude(TileSystem.MaxLatitude - 4, -TileSystem.MaxLatitude + 35, 0);
+        map.setScrollableAreaLimitLongitude(-TileSystem.MaxLongitude + 15, TileSystem.MaxLongitude + 10, 0);
+
+        MapView mv = findViewById(R.id.map);
         double mapRatio = 1; // 타일은 정사각형.
-        double zoom;
-        if (dimenRatio < mapRatio) {
-            // mapHeight to display height
-            zoom = dm.heightPixels / 256;
-        }
-        else {
-            // mapwidth to display width
-            zoom = dm.widthPixels / 256;
-        }
+        double dimenRatio = mv.getWidth() / (double) mv.getHeight(); // 화면비율
+        int longAxis = dimenRatio < mapRatio ? mv.getHeight() : mv.getWidth();
+        double zoom = longAxis / 256.0;
         double logZoom = Math.log(zoom) / Math.log(2);
 
 //        map.setMinZoomLevel(WORLDMAP_ZOOM_LEVEL );   //최소 줌 조절
         map.setMaxZoomLevel(6.0);   //최대 줌 조절
+//        map.setTilesScaledToDpi(true); //dpi에 맞게 조절
 
-   //     map.setTilesScaledToDpi(true); //dpi에 맞게 조절
-
-
-        //minimap
-        minimapOverlay = new MinimapOverlay(context,map.getTileRequestCompleteHandler());
-        minimapOverlay.setWidth(dm.widthPixels/4);
-        minimapOverlay.setHeight(dm.heightPixels/4);
+        //minimapgi
+        minimapOverlay = new MinimapOverlay(context, map.getTileRequestCompleteHandler());
+        minimapOverlay.setWidth(dm.widthPixels / 4);
+        minimapOverlay.setHeight(dm.heightPixels / 4);
         minimapOverlay.setTileSource(TileSourceFactory.BASE_OVERLAY_NL);
         minimapOverlay.setZoomDifference(3);
-        map.getOverlays().add(0,minimapOverlay);
+        map.getOverlays().add(0, minimapOverlay);
 
         mapController = map.getController();
-
 //        mapController.setZoom(WORLDMAP_ZOOM_LEVEL );
         mapController.setZoom(logZoom);
+        mapController.setCenter(new GeoPoint(40.0, 11));
 
-        mapController.setCenter(new GeoPoint(40.0,11));
+        Toast.makeText(context, dm.widthPixels+"  "+dm.heightPixels, Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(context, dm.densityDpi+" "+dm.density+"  "+dm.heightPixels, Toast.LENGTH_SHORT).show();
         MapEventsReceiver mReceiver = new MapEventsReceiver() { //화면 터치시 좌표 토스트메시지 출력, 좌표로 화면 이동
             @Override
             public boolean singleTapConfirmedHelper(GeoPoint p) {
@@ -98,6 +94,7 @@ public class MapActivity extends AppCompatActivity {
                 addMarker(p);
                 return false;
             }
+
             @Override
             public boolean longPressHelper(GeoPoint p) {    //길게 터치시
                 return false;
@@ -117,17 +114,18 @@ public class MapActivity extends AppCompatActivity {
         map.getOverlays().add(marker);
         map.invalidate(); //map refresh
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        if(map !=null)
+        if (map != null)
             map.onResume(); //osmdroid configuration refresh
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(map!=null)
+        if (map != null)
             map.onPause(); //osmdroid configuration refresh
     }
 
