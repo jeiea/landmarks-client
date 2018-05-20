d916b9c807f58cfe5dc7658ba4da3d3ed8bc85ec
Jinseog Kim <rlawlstjr4876@gmail.com>
2018-05-21 01:11:32 +0900

    GameActivity image animation

--- app/src/main/java/kr/ac/kw/coms/globealbum/game/GameActivity.java
+++ app/src/main/java/kr/ac/kw/coms/globealbum/game/GameActivity.java
@@ -2,8 +2,12 @@ package kr.ac.kw.coms.globealbum.game;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import org.osmdroid.config.Configuration;
 
@@ -14,16 +18,34 @@ import kr.ac.kw.coms.globealbum.R;
 public class GameActivity extends AppCompatActivity {
     Context context = null;
     MyMapView myMapView= null;
+    ImageView imageView=null;
+    Animation animationFadeAway;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
         //osmdroid 초기 구성
         context = getApplicationContext();
         Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
         myMapView = (MyMapView)findViewById(R.id.map);
+        imageView = (ImageView)findViewById(R.id.test_imageview);
+
+        animationFadeAway= AnimationUtils.loadAnimation(this,R.anim.fade_away);
+
+
+    }
+
+
+    //onCreate 진행 후 애니메이션 실행
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        imageView.startAnimation(GameActivity.this.animationFadeAway);
+
     }
 
     @Override
