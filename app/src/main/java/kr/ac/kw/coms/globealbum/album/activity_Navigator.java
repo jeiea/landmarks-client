84f4ba7493d7def3a3a9882c10e8b12f3c096d48
strange-1 <strange_1@daum.net>
2018-05-23 19:57:21 +0900

    Completed layout divider

--- app/src/main/java/kr/ac/kw/coms/globealbum/album/activity_Navigator.java
+++ app/src/main/java/kr/ac/kw/coms/globealbum/album/activity_Navigator.java
@@ -1,19 +1,30 @@
 package kr.ac.kw.coms.globealbum.album;
 /* 작성자: 이상훈 */
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import org.intellij.lang.annotations.Identifier;
 
 import java.util.ArrayList;
 
@@ -21,18 +32,23 @@ import kr.ac.kw.coms.globealbum.R;
 
 public class activity_Navigator extends AppCompatActivity {
     private ArrayList<String> folderList;
+    private Context context;
+    float density;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator);
 
+        context = this.context;
+        density = getResources().getDisplayMetrics().density;
         WindowManager wm = (WindowManager) getBaseContext().getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         final LinearLayout mapLayout = (LinearLayout) findViewById(R.id.MapLayout);
         Point size = new Point();
         display.getSize(size);
         mapLayout.setMinimumHeight((int) (size.y * 0.5));
+        final View Divider = (View)findViewById(R.id.Divider);
 
         View.OnDragListener mDragListener = new View.OnDragListener() {
             @Override
@@ -52,15 +68,20 @@ public class activity_Navigator extends AppCompatActivity {
                     case DragEvent.ACTION_DROP:
                         return true;
                     case DragEvent.ACTION_DRAG_ENDED:
-                        if (event.getResult())
-                            Toast.makeText(activity_Navigator.this, "Drag&Drop ended", Toast.LENGTH_SHORT).show();
+                        if (!event.getResult()) {
+                            ViewGroup.LayoutParams params = mapLayout.getLayoutParams();
+                            params.height = (int) event.getY() - (int)(density * 24) - (int)(Divider.getHeight() / 2);
+                            mapLayout.setLayoutParams(params);
+                        }
                         return true;
+
                 }
+
                 return true;
             }
         };
 
-        View Divider = (View)findViewById(R.id.Divider);
+
         Divider.setOnLongClickListener(new View.OnLongClickListener(){
             @Override
             public boolean onLongClick(View v) {
@@ -68,6 +89,7 @@ public class activity_Navigator extends AppCompatActivity {
                 return false;
             }
         });
+        Divider.setOnDragListener(mDragListener);
     }
 
     public void makeList() {
@@ -107,3 +129,5 @@ public class activity_Navigator extends AppCompatActivity {
         }
     }
 }
+
+//TODO: https://www.bignerdranch.com/blog/expand-a-recyclerview-in-four-steps/
