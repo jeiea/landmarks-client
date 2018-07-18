c636e2d7ea344fe8d60314d5aedce9230ae03f52
Sweetio <rlawlstjr4876@gmail.com>
2018-07-18 14:52:47 +0900

    change sample drawable to coor drawable

--- app/src/main/java/kr/ac/kw/coms/globealbum/diary/Diary_mapNPictures.java
+++ app/src/main/java/kr/ac/kw/coms/globealbum/diary/Diary_mapNPictures.java
@@ -11,6 +11,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+
 import java.util.ArrayList;
 
 import kr.ac.kw.coms.globealbum.R;
@@ -23,7 +28,10 @@ import kr.ac.kw.coms.globealbum.provider.PictureProvider;
 
 public class Diary_mapNPictures extends AppCompatActivity {
 
-    MyMapView mapView = null;
+    MyMapView mapView = null;   //맵뷰 인스턴스
+    MapEventsOverlay listenerOverlay; //맵 이벤트를 등록하는 오버레이
+
+
     GroupDiaryView picView = null;
     class InfoText
     {
@@ -53,43 +61,43 @@ public class Diary_mapNPictures extends AppCompatActivity {
         ArrayList<PictureGroup> elementList = new ArrayList<>();
         ArrayList<PictureProvider.Picture> elementRow = new ArrayList<>();
         Context c = getBaseContext();
-        elementRow.add(new ResourcePicture(c, R.drawable.sample0, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord0, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "0", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample1, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord1, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "1", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample2, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord2, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "2", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample3, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord3, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "3", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample4, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord4, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "4", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample5, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord5, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "5", Toast.LENGTH_SHORT).show();
             }
         }));
-        elementRow.add(new ResourcePicture(c, R.drawable.sample6, new View.OnClickListener() {
+        elementRow.add(new ResourcePicture(c, R.drawable.coord6, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Diary_mapNPictures.this, "6", Toast.LENGTH_SHORT).show();
@@ -106,7 +114,30 @@ public class Diary_mapNPictures extends AppCompatActivity {
         {
             findViewById(R.id.diary_mapNpics_EditStart).setVisibility(View.GONE);
         }
+
+
+        //마커 이벤트 등록
+        listenerOverlay = mapviewClickEventDisplay();
+        mapView.getOverlays().add(listenerOverlay);
+
+
     }
+    //경로를 보여주는 다이어리 화면에서 맵뷰를 클릭하였을 때 발생하는 이벤트
+    private MapEventsOverlay mapviewClickEventDisplay() {
+        return new MapEventsOverlay(new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {   //화면 한번 터치시
+                Toast.makeText(Diary_mapNPictures.this, "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {    //길게 터치시
+                return false;
+            }
+        });
+    }
+
 
     public void diary_onSaveClick(View view) {
         String title = ((TextView)findViewById(R.id.diary_mapNpics_EditTitle)).getText().toString();
