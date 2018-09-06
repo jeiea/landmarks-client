84a8d10443872ccd4eb53c3ac5867a1b5238b280
strange-1 <strange_1@daum.net>
2018-09-06 19:02:48 +0900

    new branch

---
+++ app/src/main/java/kr/ac/kw/coms/globealbum/diary/Diary_Edit.java
@@ -0,0 +1,15 @@
+package kr.ac.kw.coms.globealbum.diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.ac.kw.coms.globealbum.R;
+
+public class Diary_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_edit);
+    }
+}
