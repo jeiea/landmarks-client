3522dec51af6ba379984d90ba9bee42a3939ffcf
jeiea <solum5013@gmail.com>
2018-10-12 03:02:09 +0900

    Add auto login feature
    로그인이 필요하면 직접하지 말고 로그인 액티비티를 호출하기만 하면 될 듯

--- app/src/main/java/kr/ac/kw/coms/globealbum/provider/LoginActivity.kt
+++ app/src/main/java/kr/ac/kw/coms/globealbum/provider/LoginActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.layout_login.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 import kr.ac.kw.coms.globealbum.MainActivity
 import kr.ac.kw.coms.globealbum.R
+import kr.ac.kw.coms.globealbum.common.app
 import org.jetbrains.anko.contentView
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.toast
 
 class LoginActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.layout_start_loading)
@@ -18,11 +22,19 @@ class LoginActivity : AppCompatActivity() {
 
   override fun onResume() {
     super.onResume()
-    contentView?.postDelayed(::displayLogin, 1000)
+    contentView?.postDelayed(::displayLoginOrPass, 1000)
   }
 
-  private fun displayLogin() {
-    if (et_login == null) {
+  private fun displayLoginOrPass() {
+    val id = app.login?.also {
+      et_login.setText(it)
+    }
+    val pw = app.password?.also {
+      et_password.setText(it)
+    }
+    if (id != null && pw != null) {
+      launch(UI) { tryLogin(id, pw) }
+    } else {
       setContentView(R.layout.layout_login)
       btn_login.onClick { onLogin() }
     }
@@ -31,13 +43,25 @@ class LoginActivity : AppCompatActivity() {
   suspend fun onLogin() {
     val id = et_login.text.toString()
     val pass = et_password.text.toString()
-//      RemoteJava.client.login(id, pass)
+    tryLogin(id, pass)
+  }
+
+  private suspend fun tryLogin(id: String, pass: String) {
     try {
-      RemoteJava.client.login("login", "password")
+      RemoteJava.client.login(id, pass)
+      saveAutoLoginInfo(id, pass)
+
       val intent = Intent(this@LoginActivity, MainActivity::class.java)
       startActivity(intent)
     } catch (e: Throwable) {
       toast(e.toString())
     }
+  }
+
+  private fun saveAutoLoginInfo(id: String, pass: String) {
+    val autologin = cb_auto_login.isChecked
+    val rememberId = cb_remember_id.isChecked
+    app.login = if (autologin || rememberId) id else null
+    app.password = if (autologin) pass else null
   }
 }
