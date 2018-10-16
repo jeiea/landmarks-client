6643977126fbac497a58b34cca35e46427d8edb9
Sweetio <rlawlstjr4876@gmail.com>
2018-10-16 15:15:50 +0900

    divide address to country and place name. adjust font to login checkbox.

--- app/src/main/java/kr/ac/kw/coms/globealbum/game/AfterGameAdapter.java
+++ app/src/main/java/kr/ac/kw/coms/globealbum/game/AfterGameAdapter.java
@@ -3,6 +3,7 @@ package kr.ac.kw.coms.globealbum.game;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,7 +40,17 @@ public class AfterGameAdapter extends RecyclerView.Adapter<AfterGameAdapter.View
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         IPicture gamePictureInfo = gamePictureInfos.get(position);
         GlideApp.with(gameActivity).load(gamePictureInfo).into(holder.imageViewPicture);
-        holder.textViewPlace.setText(gamePictureInfo.getMeta().getAddress());
+        String str = gamePictureInfo.getMeta().getAddress();
+        int start = 0;
+        int end = str.length();
+        int firstSpace =  str.indexOf(" ");
+        if (firstSpace == -1) {
+            holder.textViewPlace.setText(str);
+        }
+        else{
+            holder.textViewCountry.setText(str.substring(start,firstSpace));
+            holder.textViewPlace.setText(str.substring(firstSpace+1,end));
+        }
     }
 
     @Override
@@ -49,12 +60,13 @@ public class AfterGameAdapter extends RecyclerView.Adapter<AfterGameAdapter.View
 
     class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageViewPicture;
-        TextView textViewPlace;
+        TextView textViewPlace,textViewCountry;
 
         ViewHolder(final View itemView) {
             super(itemView);
             imageViewPicture = itemView.findViewById(R.id.after_game_place_imageview);
             //imageViewPicture.setOnClickListener(new GameUI.PictureClickZoomingListener());      //사진 클릭 시 사진 확대
+            textViewCountry = itemView.findViewById(R.id.after_game_show_country_name);
             textViewPlace = itemView.findViewById(R.id.after_game_show_place_name);
         }
     }
