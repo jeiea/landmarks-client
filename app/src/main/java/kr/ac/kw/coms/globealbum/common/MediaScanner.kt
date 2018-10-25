5f7e72663eb58c876bf956fa2feea735be68141e
jeiea <solum5013@gmail.com>
2018-10-26 04:10:08 +0900

    Reflect file deletion on mediaScan

--- app/src/main/java/kr/ac/kw/coms/globealbum/common/MediaScanner.kt
+++ app/src/main/java/kr/ac/kw/coms/globealbum/common/MediaScanner.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Environment
+import android.provider.MediaStore
 import java.io.File
 
 
-fun getAllDrives(): ArrayList<File> {
-  val ret = ArrayList<File>()
+fun getAllDrives(): MutableList<File> {
+  val ret = mutableListOf<File>()
   // https://stackoverflow.com/q/39850004
   Environment.getExternalStorageDirectory()?.also { builtin ->
     ret.add(builtin)
@@ -24,22 +25,30 @@ fun getAllDrives(): ArrayList<File> {
 /**
  * 모든 드라이브의 DCIM, Pictures 폴더 .jpg 파일을 스캔한다
  */
-fun Context.mediaScan() {
-  val drives: ArrayList<File> = getAllDrives()
+fun Context.mediaScan() = mediaScan(null)
+
+/**
+ * 모든 드라이브의 DCIM, Pictures 폴더 .jpg 파일을 스캔한다
+ */
+fun Context.mediaScan(onComplete: Runnable?) {
+  val drives: MutableList<File> = getAllDrives()
 
   lateinit var scannerConn: MediaScannerConnection
   scannerConn = MediaScannerConnection(this, object :
     MediaScannerConnection.MediaScannerConnectionClient {
 
-    val filter = Regex(".*\\.(?:jpe?g|png|jfif)$")
+    val filter = Regex(".*\\.(?:jpe?g|jfif)$")
 
     override fun onMediaScannerConnected() {
+      val existing = getStoredImages()
+      existing.forEach { scannerConn.scanFile(it, null) }
+
       drives.forEach { drive ->
         val scanIfPicture: (File) -> Unit = {
           when {
             it.isHidden || it.isDirectory -> {
             }
-            it.isFile && filter.matches(it.name) ->
+            it.isFile && it.absolutePath !in existing && filter.matches(it.name) ->
               scannerConn.scanFile(it.absolutePath, null)
           }
         }
@@ -48,7 +57,23 @@ fun Context.mediaScan() {
       }
     }
 
-    override fun onScanCompleted(path: String?, uri: Uri?) {}
+    override fun onScanCompleted(path: String?, uri: Uri?) {
+      onComplete?.run()
+    }
   })
   scannerConn.connect()
+}
+
+fun Context.getStoredImages(): MutableList<String> {
+  val imagePaths = mutableListOf<String>()
+  val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+  val projection = arrayOf(MediaStore.MediaColumns.DATA)
+  contentResolver.query(uri, projection, null, null, null)?.use { cursor ->
+    val columnIndexData = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+    while (cursor.moveToNext()) {
+      val absolute = cursor.getString(columnIndexData)
+      imagePaths.add(absolute)
+    }
+  }
+  return imagePaths
 }
