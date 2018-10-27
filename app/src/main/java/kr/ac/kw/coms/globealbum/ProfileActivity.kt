16d479a20d1fafda011e024e4d23cf15fd90527b
jeiea <solum5013@gmail.com>
2018-10-28 01:07:46 +0900

    Bind logout button

---
+++ app/src/main/java/kr/ac/kw/coms/globealbum/ProfileActivity.kt
@@ -0,0 +1,27 @@
+package kr.ac.kw.coms.globealbum
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_profile.*
+import kr.ac.kw.coms.globealbum.common.app
+import kr.ac.kw.coms.globealbum.provider.LoginActivity
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+class ProfileActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_profile)
+    profile_logout_button.onClick {
+      app.password = null
+      startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
+      finish()
+    }
+  }
+
+  fun onClickBack(@Suppress("UNUSED_PARAMETER") _v: View) {
+    finish()
+  }
+}
---
+++ app/src/main/java/kr/ac/kw/coms/globealbum/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package kr.ac.kw.coms.globealbum
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class SignUpActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sign_up)
+  }
+
+  fun OnClickBack(_v: View) {
+    finish()
+  }
+}
