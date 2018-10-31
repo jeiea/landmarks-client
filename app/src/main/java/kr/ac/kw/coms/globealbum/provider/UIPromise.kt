f9f6d389f54f53ebd593a37636fe56c966da4292
jeiea <solum5013@gmail.com>
2018-10-31 20:37:37 +0900

    Fix UIPromise misbehavior

--- app/src/main/java/kr/ac/kw/coms/globealbum/provider/UIPromise.kt
+++ app/src/main/java/kr/ac/kw/coms/globealbum/provider/UIPromise.kt
@@ -1,15 +1,12 @@
 package kr.ac.kw.coms.globealbum.provider
 
-import kotlinx.coroutines.experimental.CoroutineScope
-import kotlinx.coroutines.experimental.Dispatchers
-import kotlinx.coroutines.experimental.Job
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.*
 
 open class UIPromise<T> : Promise<T>() {
   override fun resolve(scope: CoroutineScope, block: suspend () -> T): Job {
     return scope.launch(Dispatchers.Main) {
       try {
-        resolve(block())
+        resolve(withContext(scope.coroutineContext) { block() })
       }
       catch (e: Throwable) {
         resolve(e)
